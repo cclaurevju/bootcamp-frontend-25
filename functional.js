be42783139d05ad23ec964f11f0981aa28e78b7e
cclaurevju <Carlos.Claure@jala.university>
2025-05-16 15:50:50 -0400

    Solved functional exercise

---
+++ functional.js
@@ -0,0 +1,22 @@
+//Exercise Description
+// Create a function called customFilter that works like the built-in Array.prototype.filter.
+// The function should:
+// Take an array and a callback function as arguments.
+// Return a new array with the elements that pass the test implemented by the callback.
+
+//Example
+const numbers = [1, 2, 3, 4, 5, 6];
+
+function customFilter(numbers, condition) {
+  const evenNumbers = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (condition(numbers[i])) evenNumbers.push(numbers[i]);
+  }
+  return evenNumbers;
+}
+
+const evenNumbers = customFilter(numbers, function (n) {
+  return n % 2 === 0;
+});
+
+console.log(evenNumbers); // [2, 4, 6]
