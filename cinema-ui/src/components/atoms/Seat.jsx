8b0653bb7c203d87eb438a94eccf86831e788fd8
cclaurevju <Carlos.Claure@jala.university>
2025-06-04 21:22:08 -0400

    add seatNumber property to seat atom

--- cinema-ui/src/components/atoms/Seat.jsx
+++ cinema-ui/src/components/atoms/Seat.jsx
@@ -1,10 +1,10 @@
 import seatImg from "../../assets/seat.svg";
 import "./atoms.css";
 
-export default function Seat({ isAvailable = true }) {
+export default function Seat({ isAvailable = true, seatNumber = "N/A" }) {
   return (
     <div className="seat">
-      <p className="seat-number">A1</p>
+      <p className="seat-number">{seatNumber}</p>
       <div
         className={`seat-icon-container seat-${
           isAvailable ? "available" : "occuped"
