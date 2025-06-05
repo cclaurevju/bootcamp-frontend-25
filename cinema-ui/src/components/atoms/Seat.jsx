81d4bd2c13e5510cd041912c0f08d2658cd48282
cclaurevju <Carlos.Claure@jala.university>
2025-06-04 21:20:55 -0400

    add isAvailable property to seat

--- cinema-ui/src/components/atoms/Seat.jsx
+++ cinema-ui/src/components/atoms/Seat.jsx
@@ -1,11 +1,15 @@
 import seatImg from "../../assets/seat.svg";
 import "./atoms.css";
 
-export default function Seat() {
+export default function Seat({ isAvailable = true }) {
   return (
     <div className="seat">
       <p className="seat-number">A1</p>
-      <div className="seat-icon-container seat-occuped"></div>
+      <div
+        className={`seat-icon-container seat-${
+          isAvailable ? "available" : "occuped"
+        }`}
+      ></div>
     </div>
   );
 }
