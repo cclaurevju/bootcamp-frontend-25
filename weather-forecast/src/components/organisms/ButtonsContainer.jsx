51a215b52f5a8ee73458ff28f159df3ca3676771
cclaurevju <Carlos.Claure@jala.university>
2025-06-12 17:01:52 -0400

    add: show data

--- weather-forecast/src/components/organisms/ButtonsContainer.jsx
+++ weather-forecast/src/components/organisms/ButtonsContainer.jsx
@@ -4,8 +4,8 @@ import "./organisms.css";
 export default function ButtonsContainer({ history, search }) {
   return (
     <div className="buttons-container">
-      {history.map((city) => {
-        return <Button onClick={search(city)} label={city} />;
+      {history.map((city, index) => {
+        return <Button key={index} onClick={search(city)} label={city} />;
       })}
     </div>
   );
--- weather-forecast/src/components/organisms/Container.jsx
+++ weather-forecast/src/components/organisms/Container.jsx
@@ -48,6 +48,7 @@ export default function Container() {
           onClick={() => {
             setCity("");
             setHistory([]);
+            setData({});
           }}
         />
         <Button
@@ -58,6 +59,7 @@ export default function Container() {
         />
       </div>
       <ButtonsContainer history={history} search={search} />
+      <div>{JSON.stringify(data)}</div>
     </div>
   );
 }
