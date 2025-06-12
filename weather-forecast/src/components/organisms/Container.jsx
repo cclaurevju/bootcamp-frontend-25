eef5465686e37e324290540683002e4cc0e2825c
cclaurevju <Carlos.Claure@jala.university>
2025-06-12 16:53:52 -0400

    add: button atoms and a general container

---
+++ weather-forecast/src/components/organisms/Container.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+import Button from "../atoms/Button";
+import "./organisms.css";
+
+const mockWeatherData = {
+  "New York": {
+    temperature: "22°C",
+    humidity: "56%",
+    windSpeed: "15 km/h",
+  },
+  "Los Angeles": {
+    temperature: "27°C",
+    humidity: "45%",
+    windSpeed: "10 km/h",
+  },
+  London: {
+    temperature: "15°C",
+    humidity: "70%",
+    windSpeed: "20 km/h",
+  },
+};
+
+export default function Container() {
+  const [city, setCity] = useState("");
+  const [data, setData] = useState({});
+  const [history, setHistory] = useState([]);
+
+  const search = (inputCity) => {
+    const result = mockWeatherData[city];
+    if (result) {
+      setData(result);
+      setHistory([...history, city]);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          onChange={(e) => setCity(e.target.value)}
+          value={city}
+        />
+        <Button
+          label="Search"
+          onClick={() => {
+            search(city);
+          }}
+        />
+      </div>
+      <div></div>
+    </div>
+  );
+}
