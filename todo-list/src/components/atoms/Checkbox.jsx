6d679cf54936ff52f8b8105886ec882f93fb3224
cclaurevju <Carlos.Claure@jala.university>
2025-06-19 16:23:47 -0400

    create checkbox component

--- todo-list/src/components/atoms/Checkbox.jsx
+++ todo-list/src/components/atoms/Checkbox.jsx
@@ -1,3 +1,8 @@
-export default function Checkbox() {
-  return <div>Checkbox</div>;
+export default function Checkbox({ label, checked, onChange, id }) {
+  return (
+    <div className="flex items-center gap-2">
+      <input type="checkbox" id={id} checked={checked} onChange={onChange} />
+      <label htmlFor={id}>{label}</label>
+    </div>
+  );
 }
