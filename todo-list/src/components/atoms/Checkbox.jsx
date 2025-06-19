b7bf79e573a4c684525f64c10f7b919a254207d7
cclaurevju <Carlos.Claure@jala.university>
2025-06-19 16:24:16 -0400

    remove styles

--- todo-list/src/components/atoms/Checkbox.jsx
+++ todo-list/src/components/atoms/Checkbox.jsx
@@ -1,6 +1,6 @@
 export default function Checkbox({ label, checked, onChange, id }) {
   return (
-    <div className="flex items-center gap-2">
+    <div>
       <input type="checkbox" id={id} checked={checked} onChange={onChange} />
       <label htmlFor={id}>{label}</label>
     </div>
