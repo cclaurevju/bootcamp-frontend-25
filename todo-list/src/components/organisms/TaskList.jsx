40b35aa4424a918fe47802108edfbe097af44535
cclaurevju <Carlos.Claure@jala.university>
2025-06-19 16:39:51 -0400

    create Task list component

--- todo-list/src/components/organisms/TaskList.jsx
+++ todo-list/src/components/organisms/TaskList.jsx
@@ -1,3 +1,25 @@
-export default function TaskList() {
-  return <div>TaskList</div>;
+import Task from "../molecules/Task";
+import "./organisms.css";
+
+export default function TaskList({
+  tasksArray = [],
+  setTasksArray = () => {},
+}) {
+  const deleteTask = (id) => {
+    setTasksArray(
+      tasksArray.filter((task, index) => {
+        return index != id;
+      })
+    );
+  };
+
+  const EditTask = () => {};
+
+  return (
+    <div>
+      {tasksArray.map((task, i) => {
+        return <Task />;
+      })}
+    </div>
+  );
 }
