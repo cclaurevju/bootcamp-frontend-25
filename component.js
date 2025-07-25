5c8c9957f844bad51ca051bbdb513146ec86f61c
cclaurevju <Carlos.Claure@jala.university>
2025-07-25 16:58:03 -0400

    style: add styles to component

--- component.js
+++ component.js
@@ -4,37 +4,58 @@ class IdCard extends HTMLElement {
     const shadow = this.attachShadow({ mode: "open" });
 
     const container = document.createElement("div");
-    container.className = "container";
+    container.style.borderStyle = "solid";
+    container.style.borderWidth = "5px";
+    container.style.borderColor = "black";
+    container.style.aspectRatio = "9/10";
+    container.style.display = "flex";
+    container.style.flexDirection = "column";
+    container.style.alignItems = "center";
+    container.style.justifyContent = "space-between";
 
     const header = document.createElement("div");
-    header.className = "header";
+    header.style.width = "100%";
 
     const brand = document.createElement("img");
-    brand.className = "brand";
     brand.src = "static/brand.png";
+    brand.style.width = "100%";
     header.appendChild(brand);
 
     const body = document.createElement("div");
-    body.className = "body";
-
-    const photoWrapper = document.createElement("div");
-    photoWrapper.className = "photo-wrapper";
+    body.style.backgroundColor = "#282828";
+    body.style.width = "100%";
+    body.style.height = "100%";
+    body.style.display = "flex";
+    body.style.flexDirection = "column";
+    body.style.alignItems = "center";
+    body.style.justifyContent = "space-evenly";
 
     const photo = document.createElement("img");
-    photo.className = "photo";
     photo.src = "static/photo.png";
+    photo.style.overflow = "hidden";
+    photo.style.borderRadius = "20px";
     body.appendChild(photo);
 
     const name = document.createElement("div");
     name.textContent = "Bart Simpson";
+    name.style.textAlign = "center";
+    name.style.color = "white";
+    name.style.fontWeight = "900";
+    name.style.fontSize = "2rem";
     body.appendChild(name);
 
     const footer = document.createElement("div");
-    footer.className = "footer";
+    footer.style.width = "100%";
+
+    footer.style.backgroundColor = "#ee283f";
 
     const role = document.createElement("p");
     role.textContent = "Developer";
-    role.className = "role";
+    role.style.textAlign = "center";
+    role.style.color = "white";
+    role.style.fontWeight = "900";
+    role.style.fontSize = "2rem";
+    role.style.margin = "5%";
     footer.appendChild(role);
 
     container.appendChild(header);
