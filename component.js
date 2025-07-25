b540cd9dfff0e8d99b11a5077d3ebde96fdbe552
cclaurevju <Carlos.Claure@jala.university>
2025-07-25 16:34:14 -0400

    feat: add basic structure without style

--- component.js
+++ component.js
@@ -3,10 +3,33 @@ class IdCard extends HTMLElement {
     super();
     const shadow = this.attachShadow({ mode: "open" });
 
-    const text = document.createElement("p");
-    text.textContent = "Hello world!";
+    const container = document.createElement("div");
+    container.className = "container";
 
-    shadow.appendChild(text);
+    const header = document.createElement("div");
+    header.className = "header";
+
+    const body = document.createElement("div");
+    body.className = "body";
+
+    const photoWrapper = document.createElement("div");
+    photoWrapper.className = "photo-wrapper";
+
+    const photo = document.createElement("img");
+    photo.className = "photo";
+    body.appendChild(photo);
+
+    const name = document.createElement("div");
+    body.appendChild(name);
+
+    const footer = document.createElement("div");
+    footer.className = "footer";
+
+    container.appendChild(header);
+    container.appendChild(body);
+    container.appendChild(footer);
+
+    shadow.appendChild(container);
   }
 }
 
