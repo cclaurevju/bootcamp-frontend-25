4562631e4c27dedbf016a151d3b9632fa37ca076
cclaurevju <Carlos.Claure@jala.university>
2025-07-25 16:38:37 -0400

    feat: add information

--- component.js
+++ component.js
@@ -9,6 +9,11 @@ class IdCard extends HTMLElement {
     const header = document.createElement("div");
     header.className = "header";
 
+    const brand = document.createElement("img");
+    brand.className = "brand";
+    brand.src = "static/brand.png";
+    header.appendChild(brand);
+
     const body = document.createElement("div");
     body.className = "body";
 
@@ -17,13 +22,20 @@ class IdCard extends HTMLElement {
 
     const photo = document.createElement("img");
     photo.className = "photo";
+    photo.src = "static/photo.png";
     body.appendChild(photo);
 
     const name = document.createElement("div");
+    name.textContent = "Bart Simpson";
     body.appendChild(name);
 
     const footer = document.createElement("div");
     footer.className = "footer";
+
+    const role = document.createElement("p");
+    role.textContent = "Developer";
+    role.className = "role";
+    footer.appendChild(role);
 
     container.appendChild(header);
     container.appendChild(body);
