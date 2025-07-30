c2518b55bad20b615af19d5473101273a4641894
cclaurevju <Carlos.Claure@jala.university>
2025-07-30 14:28:31 -0400

    add fetch products

--- src/Checkout.tsx
+++ src/Checkout.tsx
@@ -1,5 +1,7 @@
-import styles from './Checkout.module.css';
-import { LoadingIcon } from './Icons';
+import { useEffect, useReducer } from "react";
+import styles from "./Checkout.module.css";
+import { LoadingIcon } from "./Icons";
+import { getProducts } from "./dataService";
 // import { getProducts } from './dataService';
 
 // You are provided with an incomplete <Checkout /> component.
@@ -20,54 +22,103 @@ import { LoadingIcon } from './Icons';
 //  - The total should reflect any discount that has been applied
 //  - All dollar amounts should be displayed to 2 decimal places
 
-
-
-const Product = ({ id, name, availableCount, price, orderedQuantity, total }) => {
+const Product = ({
+  id,
+  name,
+  availableCount,
+  price,
+  orderedQuantity,
+  total,
+}) => {
   return (
     <tr>
       <td>{id}</td>
       <td>{name}</td>
       <td>{availableCount}</td>
       <td>${price}</td>
-      <td>{orderedQuantity}</td>   
+      <td>{orderedQuantity}</td>
       <td>${total}</td>
       <td>
         <button className={styles.actionButton}>+</button>
         <button className={styles.actionButton}>-</button>
       </td>
-    </tr>    
+    </tr>
   );
-}
+};
 
+const initialState = {
+  products: [],
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_PRODUCTS": {
+      const { products } = action.payload;
+      return {
+        ...state,
+        products: [...products],
+      };
+    }
+
+    default: {
+      console.error("Unknown action type", action.type);
+      return state;
+    }
+  }
+};
 
 const Checkout = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    if (state.products.length == 0)
+      getProducts().then((res) => {
+        console.log(res);
+        dispatch({ type: "FETCH_PRODUCTS", payload: { products: res } });
+      });
+  }, []);
   return (
     <div>
-      <header className={styles.header}>        
-        <h1>Electro World</h1>        
+      <header className={styles.header}>
+        <h1>Electro World</h1>
       </header>
       <main>
-        <LoadingIcon />        
-        <table className={styles.table}>
-          <thead>
-            <tr>
-              <th>Product ID</th>
-              <th>Product Name</th>
-              <th># Available</th>
-              <th>Price</th>
-              <th>Quantity</th>
-              <th>Total</th>
-              <th></th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-          {/* Products should be rendered here */}
-          </tbody>
-        </table>
+        {state.products ? (
+          <table className={styles.table}>
+            <thead>
+              <tr>
+                <th>Product ID</th>
+                <th>Product Name</th>
+                <th># Available</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Total</th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {state.products.map((product) => {
+                return (
+                  <Product
+                    id={product.id}
+                    name={product.name}
+                    price={product.price}
+                    total={product.availableCount}
+                    availableCount={product.availableCount}
+                    orderedQuantity={0}
+                  />
+                );
+              })}
+            </tbody>
+          </table>
+        ) : (
+          <LoadingIcon />
+        )}
+
         <h2>Order summary</h2>
         <p>Discount: $ </p>
-        <p>Total: $ </p>       
+        <p>Total: $ </p>
       </main>
     </div>
   );
